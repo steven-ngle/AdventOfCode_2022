5925f4c054dbdb77540638301e0bbaec6f4ab0ce
steven-ngle <117310378+steven-ngle@users.noreply.github.com>
2022-12-04 15:11:11 +0100

    Part 2

--- AdventOfCode2022/src/days/Day1.java
+++ AdventOfCode2022/src/days/Day1.java
@@ -42,10 +42,26 @@ public class Day1 {
 			e.printStackTrace();
 		}
 		
-//		System.out.println(list);
-		
         System.out.println(Collections.max(list));
-		
+        
+        
+        // part two
+        
+        ArrayList<Integer> list2 = new ArrayList<Integer>();
+        
+        for (int i = 0; i < 3; i++) {
+        	
+        	list2.add(Collections.max(list));
+        	list.remove(Collections.max(list));
+        	
+        }
+        
+        int sum2 = 0;
+        for(int i = 0; i < list2.size(); i++)
+            sum2 += list2.get(i);
+        
+//        System.out.println(list2);
+        System.out.println(sum2);
 	}
 
 }
